b9b8121dfbccac530d20cfc7b14ad433a9179e9c
mpk75234 <mikekennedy75234@gmail.com>
2019-07-23 07:41:00 -0500

    R in CRUD

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
+var app = express();
 
 //Technically we should create a DAO for genres and books, that's slated for Q4 2019
-
 MongoClient.connect("mongodb://localhost:27017", (err, client)=>{
   err === null ? console.log('mongodb is the connected!') : console.error(err);
   let db = client.db("bookstore");
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/books', function(req, res, next) {
+  db.collection('books').find({}).toArray((err, dox)=>{
+    err === null ? console.log('books!') : console.error(err);
+    res.json(dox);
+  });
 });
 });
 
